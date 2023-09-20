786343db92804d2f6da239bc28aa5d5951c5858d
Artem <you@example.com>
2023-09-20 23:18:35 +0800

    Изменено решение задачи 8

--- Homeworks/Задача8/Program.cs
+++ Homeworks/Задача8/Program.cs
@@ -1,17 +1,25 @@
 ﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-//Условие задачи подразумевает, что пользователь должен ввести число, больше 1.
-System.Console.WriteLine("Input your number greater than 1:");
+//Подразумевается, что пользователь вводит число больше 1 или меньше -1.
+System.Console.WriteLine("Input your number greater than 1 or less than -1:");
 int num = Convert.ToInt32(Console.ReadLine());
-int i = 2;
-while (num < 2)
-{
-    System.Console.WriteLine("Your number is less than or equal to 1!");
-    System.Console.WriteLine("Input your number greater than 1:");
-    num = Convert.ToInt32(Console.ReadLine());
-}
+int i = 0;
 System.Console.WriteLine($"Even numbers from 1 to {num}: ");
-while (i <= num)
+if (num > 1)
 {
-    System.Console.Write($"{i} ");
-    i = i + 2;
+    i = 2;
+    while (i <= num)
+    {
+        System.Console.Write($"{i} ");
+        i = i + 2;
+    }
+}
+else
+{
+    i = -2;
+    while (i >= num)
+    {
+        System.Console.Write($"{i} ");
+        i = i - 2;
+    }
+
 }
