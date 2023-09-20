adb40efe37ada6095b3c58638f27f002a4fd7c59
Artem <you@example.com>
2023-09-20 22:54:32 +0800

    Домашняя работа выполнена

---
+++ Homeworks/Задача6/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+System.Console.WriteLine("Input your number:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    System.Console.WriteLine($"The number {num} is even.");
+}
+else { System.Console.WriteLine($"The number {num} is odd."); }
---
+++ Homeworks/Задача8/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//Условие задачи подразумевает, что пользователь должен ввести число, больше 1.
+System.Console.WriteLine("Input your number greater than 1:");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (num < 2)
+{
+    System.Console.WriteLine("Your number is less than or equal to 1!");
+    System.Console.WriteLine("Input your number greater than 1:");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+System.Console.WriteLine($"Even numbers from 1 to {num}: ");
+while (i <= num)
+{
+    System.Console.Write($"{i} ");
+    i = i + 2;
+}
