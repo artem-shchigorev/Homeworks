dc5ce4c5f62e85a6563896a35a327ee3692df498
Artem <you@example.com>
2023-10-01 15:22:15 +0800

    Выполнено домашнее задание к уроку 4

---
+++ Homeworks/Homeworks_Lesson4/Задача27/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int sumNumber(int num)
+{
+    int sum = 0;
+    int i = 10;
+    do
+    {
+        sum = sum + (num%i)/(i/10);
+        i = i * 10;
+    }
+    while (num/i !=0);
+    sum = sum + (num%i)/(i/10);
+    return sum;
+}
+
+System.Console.WriteLine("Введите ваше число: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine($"Cумма цифр в числе {num} = {sumNumber(num)}");
---
+++ Homeworks/Homeworks_Lesson4/Задача29/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] printArray ()
+{
+    int[] array = new int[8];
+    for(int i = 0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(0,100);
+        System.Console.Write(array[i] + " ");
+    }
+    return array;
+}
+printArray();
