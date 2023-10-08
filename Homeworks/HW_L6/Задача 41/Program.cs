6569c99949158184d996657b757fe47b16198707
Artem <you@example.com>
2023-10-08 18:19:18 +0800

    Выполнено домашнее задание семинара 6

---
+++ Homeworks/HW_L6/Задача 41/Program.cs
@@ -0,0 +1,29 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+int EnterParameters(string message)
+{
+    System.Console.WriteLine(message);
+    int parameter = Convert.ToInt32(System.Console.ReadLine());
+    return parameter;
+}
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = EnterParameters($"Введите ваше {i+1}-ое число: ");
+    }
+    return array;
+}
+void NumberOfPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i<array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    System.Console.WriteLine($"Количество положительных введенных чисел --> {count}");
+}
+int[] arr = CreateArray(EnterParameters("Введите количество вводимых чисел: "));
+NumberOfPositiveNumbers(arr);
---
+++ Homeworks/HW_L6/Задача 43/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double EnterParameters(string message)
+{
+    System.Console.WriteLine(message);
+    double parameter = Convert.ToDouble(System.Console.ReadLine());
+    return parameter;
+}
+void FindingIntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    if (b1 == 0 && b2 == 0 && k1 != k2) System.Console.WriteLine("Прямые не пересекаются ни в одной точке!");
+    else if (b1 == 0 && b2 == 0 && k1 == k2) System.Console.WriteLine("Прямые пересекаются во всех точках!");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        System.Console.WriteLine($"Прямые пересекаются в точке [{x}, {y}]");
+    }
+}
+FindingIntersectionPoint(EnterParameters("Введите b1"), EnterParameters("Введите k1"), EnterParameters("Введите b2"), EnterParameters("Введите k2"));
